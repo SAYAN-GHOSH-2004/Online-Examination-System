71928fc6a69d7d9bbb81c86a6cf401fff6267bd8
SAYAN GHOSH <sayandrive2004@gmail.com>
2025-12-27 11:14:13 +0530

    Refactor server.js for improved readability

--- backend/server.js
+++ backend/server.js
@@ -69,5 +69,6 @@ createAdmin();
 // ================= SERVER START =================
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
+
